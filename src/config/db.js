799f70a7fe26914714be4424d5c7f4eb8c2d0928
dex <dexterrgolden@gmail.com>
2016-07-28 15:11:02 -0500

    not local hpst maybe

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
-// var connectionString = 'mongodb://localhost/gimeUsers';
-//var connectionString = process.env.DB_HOST || 'mongodb://localhost/gimeUsers';
+
 var connectionString = process.env.DB_HOST
 
 
